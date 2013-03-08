57ee4a7f94cfdd472b4a6fa59a1278dbc7b5ed97
davidspotts <davidrspotts@gmail.com>
2013-03-08 09:03:25 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lyks::Application.config.secret_token = 'a127e72ee3002ec7fde20aa72ea2ceec35353023396b5ae5d2d13376aa11b6f6a8c7ba8da5167cc5ec0bc65f7d0c48e7b94dcfcf7905cfe396e3a9f2fefbc147'
